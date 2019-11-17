5a2105928d4501c0fd2bf26c029b408c50b7ab0d
Rameswar143 <ramu08078@gmail.com>
2019-11-17 12:11:07 +0530

    cocde formatted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import './App.css';
-import HomePage from './homepage.component'
+import React from "react";
+import "./App.css";
+import HomePage from "./homepage.component";
 function App() {
-  return (
-      <HomePage/>    
-  );
+  return <HomePage />;
 }
-
 export default App;
