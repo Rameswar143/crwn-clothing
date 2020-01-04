f3d1f48497b11d8aaad3a82dc6eb2fbcd2842e02
Rameswar143 <ramu08078@gmail.com>
2020-01-04 15:37:05 +0530

    app.js clened

--- src/App.js
+++ src/App.js
@@ -12,23 +12,6 @@ import { connect } from 'react-redux';
 class App extends React.Component {
 	unsubscribeFromAuth = null;
 
-	// componentDidMount() {
-	// 	const { setCurrentUser } = this.props;
-	// 	this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-	// 		if (userAuth) {
-	// 			const userRef = await createUserProfileDocument(userAuth);
-
-	// 			userRef.onSnapshot(snapShot => {
-	// 				setCurrentUser({
-	// 					id: snapShot.id,
-	// 					...snapShot.data(),
-	// 				});
-	// 			});
-	// 		}
-	// 		setCurrentUser(userAuth);
-	// 	});
-	// }
-
 	componentDidMount() {
 		const { setCurrentUser } = this.props;
 
